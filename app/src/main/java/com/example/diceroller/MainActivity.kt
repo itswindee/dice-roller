d87bedaa36e4dbeae790474758c99f185a821a05
windee <ezhu0922@gmail.com>
2024-02-12 18:10:41 -0500

    i added another twice so there are two dices now.

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -44,7 +44,6 @@ class MainActivity : ComponentActivity() {
 }
 
 
-
 @Preview
 @Composable
 fun DiceRollerApp() {
@@ -56,8 +55,10 @@ fun DiceRollerApp() {
 
 @Composable
 fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
-    var result by remember{ mutableStateOf(1)}
-    val imageResource = when(result){
+    var result1 by remember{ mutableStateOf(1)}
+    var result2 by remember{ mutableStateOf(1)}
+
+    val imageResource1 = when(result1){
         1 -> R.drawable.dice_1
         2 -> R.drawable.dice_2
         3 -> R.drawable.dice_3
@@ -65,9 +66,25 @@ fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
         5 -> R.drawable.dice_5
         else -> R.drawable.dice_6
     }
-    Column (modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
-        Image(painter = painterResource(imageResource), contentDescription = result.toString())
-        Button(onClick = { result = (1..6).random()}){
+
+    val imageResource2 = when(result2){
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
+
+    Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
+        Image(painter = painterResource(imageResource1), contentDescription = result1.toString())
+        Spacer(modifier = Modifier.height(16.dp))
+        Image(painter = painterResource(imageResource2), contentDescription = result2.toString())
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = {
+            result1 = (1..6).random()
+            result2 = (1..6).random()
+        }) {
             Text(text = stringResource(R.string.roll), fontSize = 24.sp)
         }
     }
